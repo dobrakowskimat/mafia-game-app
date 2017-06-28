48f6f81c543a24e08627cac687c6d72a78058aef
CORP\mdobrakowski <madobrakowski@infusion.com>
2017-06-28 19:01:18 +0200

    changes

---
+++ mafia-game-app/src/app/components/events/mock-events.ts
@@ -0,0 +1,17 @@
+import { Event } from './event';
+
+export const Events: Event[] = [
+      { id: 1, title: 'smierc glupcom', description: 'ktos traci all i cos tam sie dzieje' },
+      { id: 2, title: 'haha event', description: 'cos innego i fajnego sie dzieje' },
+      { id: 3, title: 'ups take this', description: 'margaryna i cos' },
+      { id: 4, title: 'zyjemy?', description: 'ktos traci all i cos tam sie dzieje' },
+      { id: 5, title: 'mafia jol', description: 'ktos traci all i cos tam sie dzieje' },
+      { id: 6, title: 'smierc glupcom', description: '12323 123 ' },
+      { id: 7, title: 'smierc glupcom', description: 'asd asd asd ' },
+      { id: 8, title: 'smierc glupcom', description: '123 123 43534  df gd' },
+      { id: 9, title: 'smierc glupcom', description: 'description bardzo' },
+      { id: 10, title: 'smierc glupcom', description: 'jakis tam opis iwentu' },
+      { id: 11, title: 'smierc glupcom', description: 'ktos traci all i cos tam sie dzieje' },
+      { id: 12, title: 'smierc glupcom', description: 'ktos traci all i cos tam sie dzieje' },
+      { id: 13, title: 'smierc glupcom', description: 'ktos traci all i cos tam sie dzieje' },
+    ];
