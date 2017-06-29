14f0bd6d1e66d0bec3150bde208a08a1c2299927
CORP\mdobrakowski <madobrakowski@infusion.com>
2017-06-29 17:13:34 +0200

    rozpoczecie gry

--- mafia-game-app/src/app/components/dashboard/dashboard.component.ts
+++ mafia-game-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Locals } from './../../mock-locals';
+import { Local } from './../../local';
 
 @Component({
     selector: 'dashboard',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: [ './dashboard.component.scss' ]
 })
 export class DashboardComponent implements OnInit {
+  public firstOpenLocal: Local;
+  public secondOpenLocal: Local;
+  public currentTurn: number = 0;
+  public endTurn: number = 0;
+  private status: string = "game start"; 
 
   ngOnInit(): void {
+  }
+  prepareMap(): void {
+    let openLocalId = Math.floor(Math.random() * 7);  
+    this.firstOpenLocal = Locals[openLocalId];
+    let openLocalId2 = Math.floor(Math.random() * 7);
+    if (openLocalId !== openLocalId2) {
+      this.secondOpenLocal = Locals[openLocalId2];
+    } else {
+      this.secondOpenLocal = Locals[(openLocalId2 + 1)];
+    }
+  }
+  startGame(): void {
+    this.endTurn = Math.floor(Math.random() * 5) + 10;
+    this.status = "turn start";
+    this.currentTurn = 1;
   }
 }
---
+++ mafia-game-app/src/app/mock-locals.ts
@@ -0,0 +1,13 @@
+import { Local } from './local';
+
+export const Locals: Local[] = [
+      { id: 1, district: 1, localNumber: 1},
+      { id: 2, district: 1, localNumber: 2},
+      { id: 3, district: 1, localNumber: 3},
+      { id: 4, district: 1, localNumber: 4},
+      { id: 5, district: 2, localNumber: 1},
+      { id: 6, district: 2, localNumber: 2},
+      { id: 7, district: 2, localNumber: 3},
+      { id: 8, district: 2, localNumber: 4},
+
+    ];
